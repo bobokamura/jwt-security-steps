db264a3f87721206ccaccce9498c4a6beffa6ce5
bobokamura <roberto.okamura@cotefacil.com>
2023-01-25 18:09:25 -0300

    Implement the register method

--- src/main/java/com/cotefacil/security/auth/AuthenticationService.java
+++ src/main/java/com/cotefacil/security/auth/AuthenticationService.java
@@ -1,15 +1,36 @@
 package com.cotefacil.security.auth;
 
+import com.cotefacil.security.config.JwtService;
+import com.cotefacil.security.repository.UserRepository;
+import com.cotefacil.security.user.Role;
+import com.cotefacil.security.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User
+            .builder()
+            .firstName(request.getFirstname())
+            .lastName(request.getLastname())
+            .email(request.getEmail())
+            .password(passwordEncoder.encode(request.getPassword()))
+            .role(Role.USER)
+            .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+            .builder()
+            .token(jwtToken)
+            .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
