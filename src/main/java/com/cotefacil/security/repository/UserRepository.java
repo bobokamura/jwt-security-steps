34652ecadfb57b4ba726de639913d651d581f569
bobokamura <roberto.okamura@cotefacil.com>
2023-01-25 15:19:07 -0300

    UserRepository created

---
+++ src/main/java/com/cotefacil/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.cotefacil.security.repository;
+
+import com.cotefacil.security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
