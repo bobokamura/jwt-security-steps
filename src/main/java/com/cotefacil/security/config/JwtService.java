1ef376b328992b15cba999155ad8da92f486c089
bobokamura <roberto.okamura@cotefacil.com>
2023-01-25 16:20:01 -0300

    Implements the getSignInKey method and created secret on properties

--- src/main/java/com/cotefacil/security/config/JwtService.java
+++ src/main/java/com/cotefacil/security/config/JwtService.java
@@ -2,10 +2,18 @@ package com.cotefacil.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
 
     public String extractUsername(String token) {
         return null;
@@ -17,5 +25,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
