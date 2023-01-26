fede06443b8ca181a8ab5ce93ed4ffd488730ccd
bobokamura <roberto.okamura@cotefacil.com>
2023-01-26 10:01:46 -0300

    Tests done!

--- src/main/java/com/cotefacil/security/demo/DemoController.java
+++ src/main/java/com/cotefacil/security/demo/DemoController.java
@@ -1,13 +1,12 @@
 package com.cotefacil.security.demo;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/demo-controller")
 public class DemoController {
 
     @GetMapping
